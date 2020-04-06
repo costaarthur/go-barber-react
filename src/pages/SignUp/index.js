8fd140e44e7a5f744db356387aacecb0f44f47a7
costaarthur <costaarthur@hotmail.com>
2020-04-06 15:48:39 -0300

    css reducer saga reactotron

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '../../assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email()
+    .required('Insira um e-mail válido'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
-    <div />
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
   );
 }
